faa9d504527145f4e32d12a555c90cc31d38bdbe
ray <rvy5009@gmail.com>
2020-03-02 14:16:34 -0500

    practice php

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+  // $name = "some string";
+  // $string = 40;
+  // echo $name;
+  // echo $string;
+  // $x = 40.3;
+  // $x = false;
+  // var_dump($x);
+  $street = array("abc", "def");
+  $qwe = array(
+    "name" => "John",
+    "phone" => "8675309",
+    "email" => "ray@ray.com"
+  );
+  // print $street[0]
+  // print $qwe["name"];
+  echo "<pre>";
+  
+  print_r($qwe)
+
+  
+
+
+
+
+?>
+
